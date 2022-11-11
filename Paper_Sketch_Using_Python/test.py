3c2395a8b99862bb4984f1a239d2614d32b45c82
Randall J. Russell Hernadez <rjrussellh03@gmail.com>
2022-11-10 20:58:06 -0600

    GUI for collecting path of img

--- Paper_Sketch_Using_Python/test.py
+++ Paper_Sketch_Using_Python/test.py
@@ -1,111 +1,27 @@
-import gi
+from tkinter import *
+from tkinterdnd2 import *
 
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk, GdkPixbuf
+def drop(event):
+    var.set(event.data)
 
-(TARGET_ENTRY_TEXT, TARGET_ENTRY_PIXBUF) = range(2)
-(COLUMN_TEXT, COLUMN_PIXBUF) = range(2)
+ws = TkinterDnD.Tk()
+ws.title('Pencil Sketch A Image')
+ws.geometry('300x200')
+ws.config(bg='#fcba03')
 
-DRAG_ACTION = Gdk.DragAction.COPY
+var = StringVar()
+Label(ws, text='Path of the Folder', bg='#fcba03').pack(anchor=NW, padx=10)
+e_box = Entry(ws, textvar=var, width=80)
+e_box.pack(fill=X, padx=10)
+e_box.drop_target_register(DND_FILES)
+e_box.dnd_bind('<<Drop>>', drop)
 
+lframe = LabelFrame(ws, text='Instructions', bg='#fcba03')
+Label(
+    lframe, 
+    bg='#fcba03',
+    text='Drag and drop the folder \nof your choice in the above text field.\n You will notice a path over there.'
+    ).pack(fill=BOTH, expand=True)
+lframe.pack(fill=BOTH, expand=True, padx=10, pady=10)
 
-class DragDropWindow(Gtk.Window):
-    def __init__(self):
-        super().__init__(title="Drag and Drop Demo")
-
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
-
-        hbox = Gtk.Box(spacing=12)
-        vbox.pack_start(hbox, True, True, 0)
-
-        self.iconview = DragSourceIconView()
-        self.drop_area = DropArea()
-
-        hbox.pack_start(self.iconview, True, True, 0)
-        hbox.pack_start(self.drop_area, True, True, 0)
-
-        button_box = Gtk.Box(spacing=6)
-        vbox.pack_start(button_box, True, False, 0)
-
-        image_button = Gtk.RadioButton.new_with_label_from_widget(None, "Images")
-        image_button.connect("toggled", self.add_image_targets)
-        button_box.pack_start(image_button, True, False, 0)
-
-        text_button = Gtk.RadioButton.new_with_label_from_widget(image_button, "Text")
-        text_button.connect("toggled", self.add_text_targets)
-        button_box.pack_start(text_button, True, False, 0)
-
-        self.add_image_targets()
-
-    def add_image_targets(self, button=None):
-        targets = Gtk.TargetList.new([])
-        targets.add_image_targets(TARGET_ENTRY_PIXBUF, True)
-
-        self.drop_area.drag_dest_set_target_list(targets)
-        self.iconview.drag_source_set_target_list(targets)
-
-    def add_text_targets(self, button=None):
-        self.drop_area.drag_dest_set_target_list(None)
-        self.iconview.drag_source_set_target_list(None)
-
-        self.drop_area.drag_dest_add_text_targets()
-        self.iconview.drag_source_add_text_targets()
-
-
-class DragSourceIconView(Gtk.IconView):
-    def __init__(self):
-        Gtk.IconView.__init__(self)
-        self.set_text_column(COLUMN_TEXT)
-        self.set_pixbuf_column(COLUMN_PIXBUF)
-
-        model = Gtk.ListStore(str, GdkPixbuf.Pixbuf)
-        self.set_model(model)
-        self.add_item("Item 1", "image-missing")
-        self.add_item("Item 2", "help-about")
-        self.add_item("Item 3", "edit-copy")
-
-        self.enable_model_drag_source(Gdk.ModifierType.BUTTON1_MASK, [], DRAG_ACTION)
-        self.connect("drag-data-get", self.on_drag_data_get)
-
-    def on_drag_data_get(self, widget, drag_context, data, info, time):
-        selected_path = self.get_selected_items()[0]
-        selected_iter = self.get_model().get_iter(selected_path)
-
-        if info == TARGET_ENTRY_TEXT:
-            text = self.get_model().get_value(selected_iter, COLUMN_TEXT)
-            data.set_text(text, -1)
-        elif info == TARGET_ENTRY_PIXBUF:
-            pixbuf = self.get_model().get_value(selected_iter, COLUMN_PIXBUF)
-            data.set_pixbuf(pixbuf)
-
-    def add_item(self, text, icon_name):
-        pixbuf = Gtk.IconTheme.get_default().load_icon(icon_name, 16, 0)
-        self.get_model().append([text, pixbuf])
-
-
-class DropArea(Gtk.Label):
-    def __init__(self):
-        Gtk.Label.__init__(self)
-        self.set_label("Drop something on me!")
-        self.drag_dest_set(Gtk.DestDefaults.ALL, [], DRAG_ACTION)
-
-        self.connect("drag-data-received", self.on_drag_data_received)
-
-    def on_drag_data_received(self, widget, drag_context, x, y, data, info, time):
-        if info == TARGET_ENTRY_TEXT:
-            text = data.get_text()
-            print("Received text: %s" % text)
-
-        elif info == TARGET_ENTRY_PIXBUF:
-            pixbuf = data.get_pixbuf()
-            width = pixbuf.get_width()
-            height = pixbuf.get_height()
-
-            print("Received pixbuf with width %spx and height %spx" % (width, height))
-
-
-win = DragDropWindow()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+ws.mainloop()
