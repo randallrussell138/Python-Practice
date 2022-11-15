a207d7bf357e3a37900e6beedaa13d7a00f7f9d8
Randall Russell <rjrussellh03@gmail.com>
2022-11-14 23:48:04 -0600

    added questions

--- Quiz_Game/main.py
+++ Quiz_Game/main.py
@@ -1,8 +1,8 @@
 print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? (Y/N) ")
-
-if playing.lower != "Y":
+# .lower changes answer to lowercase so all casing will work
+if playing.lower != "y":
     quit()
 
 print("Okay lets play :)")
@@ -12,19 +12,48 @@ question = 0
 percentage_correct = 0
 
 def score_statement() :
-    print("You got ",int(point)," points")
+    # str() changes int to str to put into print for joining words.
+    print("You got ",str(point)," points")
     percentage_correct = (point/question) *100
-    print(percentage_correct)
+    print("You got"+ str(percentage_correct)+ "% correct!")
     return 
 
+#question 0
 answer = input("What does CPU stand for? ")
-
 if answer.lower == "central processing unit":
     print("Correct!")
     point += 1
 else:
     print("Incorrect!")
-
 question += 1
+score_statement()
 
+#question 1
+answer = input("What does GPU stand for? ")
+if answer.lower == "graphic processing unit":
+    print("Correct!")
+    point += 1
+else:
+    print("Incorrect!")
+question += 1
+score_statement()
+
+#question 2
+answer = input("What does PSU stand for? ")
+if answer.lower == "power supply":
+    print("Correct!")
+    point += 1
+else:
+    print("Incorrect!")
+question += 1
+score_statement()
+
+#question 3
+answer = input("What does RAM stand for? ")
+if answer.lower == "random access memory":
+    print("Correct!")
+    point += 1
+else:
+    print("Incorrect!")
+question += 1
 score_statement()
